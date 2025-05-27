631ad2b1e52b5a32f88ccb2c2207788189e7715f
3veryday <hyunseo.3veryday.ji@gmail.com>
2025-05-27 17:51:52 +0900

    npm run build folder added

--- SAA-C03-QUIZ/vite.config.js
+++ SAA-C03-QUIZ/vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'SAA-C03 Quiz',
+        short_name: 'QuizApp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#007BFF',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
